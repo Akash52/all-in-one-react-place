9a32b00bc9d88f0375042871d336fb22beb0b5aa
Akash Chauhan <ac8572611@gmail.com>
2023-05-15 14:56:49 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,175 @@
+export const Pas_Compute = [
+  {
+    sw: '35.59%',
+    sw_mbol: '51.69%',
+  },
+  {
+    sw: '35.59%',
+    sw_mbol: '51.69%',
+  },
+];
+export const Alb_Bandwidth = [
+  {
+    sw: '35.59gbps',
+    sw_mbol: '51.69gbps',
+  },
+  {
+    sw: '35.59gbps',
+    sw_mbol: '51.69gbps',
+  },
+];
+export const Nsxt_Bandwidth = [
+  {
+    SW: {
+      Eth0: '1.59 Gbps Out, 1.99 Gbps In',
+      Eth2: '1.78 Gbps Out, 1.93 Gbps In',
+    },
+    'SW MBOL': {
+      Eth0: '1.11 Gbps Out, 1.19 Gbps In',
+      Eth2: '1.10 Gbps Out, 1.20 Gbps In',
+    },
+  },
+  {
+    SW: {
+      Eth0: '2.16 Gbps Out, 1.65 Gbps In',
+      Eth2: '1.98 Gbps Out, 1.74 Gbps In',
+    },
+    'SW MBOL': {
+      Eth0: '2.77 Gbps Out, 2.50 Gbps In',
+      Eth2: '2.79 Gbps Out, 2.52 Gbps In',
+    },
+  },
+  {
+    SW: {
+      Eth0: '1.59 Gbps Out, 1.99 Gbps In',
+      Eth2: '1.78 Gbps Out, 1.93 Gbps In',
+    },
+    'SW MBOL': {
+      Eth0: '1.11 Gbps Out, 1.19 Gbps In',
+      Eth2: '1.10 Gbps Out, 1.20 Gbps In',
+    },
+  },
+];
+
+export const firewall = [
+  {
+    CCX01: '35%',
+    CCX301: {
+      appInterface: '45%',
+      splunkInterface: '60%',
+    },
+  },
+  {
+    CCX01: '32%',
+    CCX301: {
+      appInterface: '46%',
+      splunkInterface: '62%',
+    },
+  },
+  {
+    CCX01: '32%',
+    CCX301: {
+      appInterface: '43%',
+      splunkInterface: '57%',
+    },
+  },
+];
+
+export const ai_count = [
+  {
+    SW: {
+      totalMs: 1805,
+      totalMsInstances: 10069,
+    },
+    'SW MBOL': {
+      totalMs: 776,
+      totalMsInstances: 6403,
+    },
+  },
+  {
+    SW: {
+      totalMs: 1809,
+      totalMsInstances: 10050,
+    },
+    'SW MBOL': {
+      totalMs: 775,
+      totalMsInstances: 6402,
+    },
+  },
+  {
+    SW: {
+      totalMs: 1861,
+      totalMsInstances: 10200,
+    },
+    'SW MBOL': {
+      totalMs: 793,
+      totalMsInstances: 6342,
+    },
+  },
+];
+
+export const gemfire_fintech = [
+  {
+    SW: {
+      reads: '60.3K',
+      writes: '9.1K',
+    },
+    'SW MBOL': {
+      reads: '142.3K',
+      writes: '13.1K',
+    },
+  },
+  {
+    SW: {
+      reads: '69.1K',
+      writes: '11.5K',
+    },
+    'SW MBOL': {
+      reads: '158.9K',
+      writes: '14.4K',
+    },
+  },
+  {
+    SW: {
+      reads: '62.3K',
+      writes: '9.6K',
+    },
+    'SW MBOL': {
+      reads: '150.3K',
+      writes: '13.0K',
+    },
+  },
+];
+
+export const gemfire_ibs = [
+  {
+    SW: {
+      reads: '16.3K',
+      writes: '2.3K',
+    },
+    'SW MBOL': {
+      reads: '28.6K',
+      writes: '4.1K',
+    },
+  },
+  {
+    SW: {
+      reads: '16.6K',
+      writes: '2.5K',
+    },
+    'SW MBOL': {
+      reads: '31.1K',
+      writes: '4K',
+    },
+  },
+  {
+    SW: {
+      reads: '14.8K',
+      writes: '2.2K',
+    },
+    'SW MBOL': {
+      reads: '26.6K',
+      writes: '4.1K',
+    },
+  },
+]
